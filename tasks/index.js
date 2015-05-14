19f0857b625cae05092e5a7fb364a0b2b48a92a2
PublicFess <publicfess@gmail.com>
2015-05-14 22:02:24 +0300

    removed gulp watch. added chokidar.

--- tasks/index.js
+++ tasks/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var gulp = require('gulp')
-  , runSequence = require('run-sequence');
+  , runSequence = require('run-sequence')
+  , webserver = require('browser-sync')
+  , async = require('async')
+  , chokidar = require('chokidar');
 
 require('./images');
 require('./jade');
@@ -9,7 +12,6 @@ require('./scripts');
 require('./misc');
 require('./stylus');
 require('./clean');
-require('./webserver');
 
 gulp.task('build', function (cb) {
   runSequence(
@@ -22,21 +24,74 @@ gulp.task('build', function (cb) {
       'miscBuild'], cb);
 });
 
-gulp.task('watch', function (cb) {
-  runSequence(
-    'cleanWatch',
-    ['stylusWatch',
-      'jadeWatch',
-      'jsWatch',
-      'jsLibsWatch',
-      'imagesWatch',
-      'miscWatch'],
-    'webserver', function() {
-      gulp.watch('assets/static/css/**/*.styl', ['stylusWatch']);
-      gulp.watch('assets/views/**/*.jade', ['jadeWatch']);
-      gulp.watch('assets/static/js/**/*', ['jsWatch']);
-      gulp.watch('assets/static/js/libs/*', ['jsLibsWatch']);
-      gulp.watch('assets/static/img/**/*', ['imagesWatch']);
-      gulp.watch('assets/static/misc/**/*', ['miscWatch']);
-    });
+gulp.task('watch:setWatchers', ['cleanWatch'], function (cb) {
+  // css watcher
+  var query = [];
+  query.push(function (cb) {
+    chokidar.watch('assets/static/css/**/*')
+      .on('ready', function () {
+        console.log('Stylesheets files watched');
+        cb();
+      })
+      .on('all', function () {
+        gulp.start('stylusWatch');
+      });
+  });
+
+  // html watcher
+  query.push(function (cb) {
+    chokidar.watch('assets/views/**/*')
+      .on('ready', function () {
+        console.log('HTML files watched');
+        cb();
+      })
+      .on('all', function () {
+        gulp.start('jadeWatch');
+      });
+  });
+  // js watcher
+  query.push(function (cb) {
+    chokidar.watch('assets/static/js/**/*')
+      .on('ready', function () {
+        console.log('JavaScript files watched');
+        cb();
+      })
+      .on('all', function () {
+        gulp.start('jsWatch');
+        gulp.start('jsLibsWatch');
+      });
+  });
+  // img watcher
+  query.push(function (cb) {
+    chokidar.watch('assets/static/img/**/*')
+      .on('ready', function () {
+        console.log('Image files watched');
+        cb();
+      })
+      .on('all', function () {
+        gulp.start('imagesWatch');
+      });
+  });
+  // misc watcher
+  query.push(function (cb) {
+    chokidar.watch('assets/static/misc/**/*')
+      .on('ready', function () {
+        console.log('Misc files watched');
+        cb();
+      })
+      .on('all', function () {
+        gulp.start('miscWatch');
+      });
+  });
+  async.parallel(query, cb)
 });
+
+gulp.task('webserver', ['watch:setWatchers'], function () {
+  return webserver({
+    server: {
+      baseDir: './site'
+    }
+  });
+});
+
+gulp.task('watch', ['webserver']);
